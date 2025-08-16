09667e26185def30d2bfae2d4d2a2b88a87dc122
AlisonA-88 <alison.aird@gmail.com>
2025-08-16 23:00:08 +0100

    Updated CSS & added API to content

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
 function searchCity(city) {
   let apiKey = "2da524bt368f04d046ed72d4c1o50bad";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=2da524bt368f04d046ed72d4c1o50bad`;
-  console.log(apiUrl);
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
